c84cb52b949006f44479cebe41174d944658c1c6
Emily Harrison <ekharrison.art@gmail.com>
2022-04-29 11:53:54 -0600

    bonus mission done

---
+++ src/test/BonusBinarySearchTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import main.BonusBinarySearch;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BonusBinarySearchTest {
+
+    @Test
+    public void firstIntInArrayReturnsIndex() {
+        int[] sortedArr = {1, 5, 7, 23, 45};
+        assertEquals(0, BonusBinarySearch.binarySearch(sortedArr, 1));
+    }
+
+    @Test
+    public void lastIntInArrayReturnsIndex() {
+        int[] sortedArr = {1, 5, 7, 23, 45};
+        assertEquals(4, BonusBinarySearch.binarySearch(sortedArr, 45));
+    }
+}
